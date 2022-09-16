368266d5e3956b0900f35e9acd3b577100d31de7
jyothivmatam <113531577+jyothivmatam@users.noreply.github.com>
2022-09-16 18:31:38 +0530

    Add files via upload

---
+++ Second largest number in an array/Second largest number in an array/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+
+   public class Program
+{
+    public static void Main()
+
+    {
+    int[] array = { 50, 20, 18, 16, 17, 46, 58, 23 };
+        Array.Sort(array);
+        Array.Reverse(array);
+        Console.WriteLine("Second Highest Value in Array " + array[1]);
+       foreach (var result in array)
+        {
+            Console.Write(result + " ");
+        }
+    }
+}
